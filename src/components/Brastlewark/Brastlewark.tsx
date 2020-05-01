0a9103785802f21bda41d60b8a5e5c4484ccceda
Iván López Medina <ivan.lopez.medina.ilm@gmail.com>
2020-05-01 13:50:36 +0200

    feat(Brastlewark): Connect redux to main city page

--- src/components/Brastlewark/Brastlewark.tsx
+++ src/components/Brastlewark/Brastlewark.tsx
@@ -1,13 +1,46 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {connect} from 'react-redux';
 import Header from '../Header/Header';
+import Spinner from '../common/Spinner';
+import {loadDwarves} from '../../redux/actions/dwarvesActions';
 
-const Brastlewark = () => {
+const Brastlewark = ({dwarves, loadDwarves, loading}) => {
+  useEffect(() => {
+    if (dwarves.length === 0) {
+      loadDwarves().catch((error) => {
+        alert('Loading dwarves failed' + error);
+      });
+    }
+  });
+
+  const handle = () => {
+    debugger;
+  };
   return (
     <>
-      <Header />
-      <div>Brastlewark city main page!</div>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <>
+          <Header />
+          <div>Brastlewark city main page!</div>
+          <button onClick={handle}>dwarrfff</button>
+          {JSON.stringify(dwarves)}
+        </>
+      )}
     </>
   );
 };
 
-export default Brastlewark;
+const mapStateToProps = (state) => {
+  return {
+    dwarves: state.dwarves,
+    loading: state.apiCallsInProgress > 0,
+  };
+};
+
+const mapDispatchToProps = {
+  loadDwarves,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Brastlewark);
