4d77a3051a1afb185eea701f2733634da98cbc3c
Iván López Medina <ivan.lopez.medina.ilm@gmail.com>
2020-05-01 12:33:01 +0200

    feat(Redux): setup for redux and store

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom';
 import './index.scss';
 import App from './App';
+import configureStore from './redux/configureStore';
+import {Provider} from 'react-redux';
+
+const store = configureStore();
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
